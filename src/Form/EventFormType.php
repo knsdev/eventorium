9464517bc6d8bd8b7a89d4f9b45d4be171dd6da7
Kim <kimniklas.schlueter@gmail.com>
2025-08-15 11:54:01 +0200

    Make CRUD for Event entity via command

---
+++ src/Form/EventFormType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use App\Entity\Event;
+use App\Entity\EventType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventFormType extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+    $builder
+      ->add('name')
+      ->add('startTime')
+      ->add('image')
+      ->add('capacity')
+      ->add('contactEmail')
+      ->add('contactPhoneNumber')
+      ->add('url')
+      ->add('address', EntityType::class, [
+        'class' => Address::class,
+        'choice_label' => 'id',
+      ])
+      ->add('type', EntityType::class, [
+        'class' => EventType::class,
+        'choice_label' => 'name',
+      ])
+    ;
+  }
+
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+    $resolver->setDefaults([
+      'data_class' => Event::class,
+    ]);
+  }
+}
