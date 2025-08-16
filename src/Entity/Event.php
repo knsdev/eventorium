f6bf02482dd4a46b96b957201b75fe29a1c2d651
Kim <kimniklas.schlueter@gmail.com>
2025-08-16 14:51:23 +0200

    Allow Event.type and Event.address to be null
    Make the foreign keys to EventType and Address nullable in the Event entity.
    Add onDelete: "SET NULL" to prevent errors when deleting an EventType or Address
    that is still referenced by an Event.
    
    Update HTML templates to handle cases where Event.type or Event.address is null.

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -9,180 +9,180 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: EventRepository::class)]
 class Event
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  private ?int $id = null;
 
-    #[ORM\Column(length: 100)]
-    private ?string $name = null;
+  #[ORM\Column(length: 100)]
+  private ?string $name = null;
 
-    #[ORM\Column]
-    private ?\DateTime $startTime = null;
+  #[ORM\Column]
+  private ?\DateTime $startTime = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $image = null;
+  #[ORM\Column(length: 255, nullable: true)]
+  private ?string $image = null;
 
-    #[ORM\Column]
-    private ?int $capacity = null;
+  #[ORM\Column]
+  private ?int $capacity = null;
 
-    #[ORM\Column(length: 100)]
-    private ?string $contactEmail = null;
+  #[ORM\Column(length: 100)]
+  private ?string $contactEmail = null;
 
-    #[ORM\Column(length: 50, nullable: true)]
-    private ?string $contactPhoneNumber = null;
+  #[ORM\Column(length: 50, nullable: true)]
+  private ?string $contactPhoneNumber = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $url = null;
+  #[ORM\Column(length: 255)]
+  private ?string $url = null;
 
-    #[ORM\ManyToOne]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Address $address = null;
+  #[ORM\ManyToOne]
+  #[ORM\JoinColumn(nullable: true, onDelete: "SET NULL")]
+  private ?Address $address = null;
 
-    #[ORM\ManyToOne]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?EventType $type = null;
+  #[ORM\ManyToOne]
+  #[ORM\JoinColumn(nullable: true, onDelete: "SET NULL")]
+  private ?EventType $type = null;
 
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $description = null;
+  #[ORM\Column(type: Types::TEXT)]
+  private ?string $description = null;
 
-    #[ORM\Column]
-    private ?\DateTime $endTime = null;
+  #[ORM\Column]
+  private ?\DateTime $endTime = null;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
+  public function getName(): ?string
+  {
+    return $this->name;
+  }
 
-    public function setName(string $name): static
-    {
-        $this->name = $name;
+  public function setName(string $name): static
+  {
+    $this->name = $name;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getStartTime(): ?\DateTime
-    {
-        return $this->startTime;
-    }
+  public function getStartTime(): ?\DateTime
+  {
+    return $this->startTime;
+  }
 
-    public function setStartTime(\DateTime $startTime): static
-    {
-        $this->startTime = $startTime;
+  public function setStartTime(\DateTime $startTime): static
+  {
+    $this->startTime = $startTime;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getImage(): ?string
-    {
-        return $this->image;
-    }
+  public function getImage(): ?string
+  {
+    return $this->image;
+  }
 
-    public function setImage(?string $image): static
-    {
-        $this->image = $image;
+  public function setImage(?string $image): static
+  {
+    $this->image = $image;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getCapacity(): ?int
-    {
-        return $this->capacity;
-    }
+  public function getCapacity(): ?int
+  {
+    return $this->capacity;
+  }
 
-    public function setCapacity(int $capacity): static
-    {
-        $this->capacity = $capacity;
+  public function setCapacity(int $capacity): static
+  {
+    $this->capacity = $capacity;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getContactEmail(): ?string
-    {
-        return $this->contactEmail;
-    }
+  public function getContactEmail(): ?string
+  {
+    return $this->contactEmail;
+  }
 
-    public function setContactEmail(string $contactEmail): static
-    {
-        $this->contactEmail = $contactEmail;
+  public function setContactEmail(string $contactEmail): static
+  {
+    $this->contactEmail = $contactEmail;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getContactPhoneNumber(): ?string
-    {
-        return $this->contactPhoneNumber;
-    }
+  public function getContactPhoneNumber(): ?string
+  {
+    return $this->contactPhoneNumber;
+  }
 
-    public function setContactPhoneNumber(?string $contactPhoneNumber): static
-    {
-        $this->contactPhoneNumber = $contactPhoneNumber;
+  public function setContactPhoneNumber(?string $contactPhoneNumber): static
+  {
+    $this->contactPhoneNumber = $contactPhoneNumber;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getUrl(): ?string
-    {
-        return $this->url;
-    }
+  public function getUrl(): ?string
+  {
+    return $this->url;
+  }
 
-    public function setUrl(string $url): static
-    {
-        $this->url = $url;
+  public function setUrl(string $url): static
+  {
+    $this->url = $url;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getAddress(): ?Address
-    {
-        return $this->address;
-    }
+  public function getAddress(): ?Address
+  {
+    return $this->address;
+  }
 
-    public function setAddress(?Address $address): static
-    {
-        $this->address = $address;
+  public function setAddress(?Address $address): static
+  {
+    $this->address = $address;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getType(): ?EventType
-    {
-        return $this->type;
-    }
+  public function getType(): ?EventType
+  {
+    return $this->type;
+  }
 
-    public function setType(?EventType $type): static
-    {
-        $this->type = $type;
+  public function setType(?EventType $type): static
+  {
+    $this->type = $type;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
+  public function getDescription(): ?string
+  {
+    return $this->description;
+  }
 
-    public function setDescription(string $description): static
-    {
-        $this->description = $description;
+  public function setDescription(string $description): static
+  {
+    $this->description = $description;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getEndTime(): ?\DateTime
-    {
-        return $this->endTime;
-    }
+  public function getEndTime(): ?\DateTime
+  {
+    return $this->endTime;
+  }
 
-    public function setEndTime(\DateTime $endTime): static
-    {
-        $this->endTime = $endTime;
+  public function setEndTime(\DateTime $endTime): static
+  {
+    $this->endTime = $endTime;
 
-        return $this;
-    }
+    return $this;
+  }
 }
